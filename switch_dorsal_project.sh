8e1f7b48aac8e4db3478af3a4e765f1e1dae2512
Roman Putanowicz <R.Putanowicz@L5.pk.edu.pl>
2016-01-19 13:52:56 +0100

    Added simple script to reling local.cfg to project specific files

---
+++ switch_dorsal_project.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -a
+
+link_local_cfg() {
+  LCFG=local_${HOSTNAME}_${PROJECT}.cfg
+  if [ -f ${LCFG} ]; then
+    mv local.cfg local.cfg_old
+    ln -s ${LCFG} local.cfg
+  else
+    echo Error file $LCFG does not exists
+  fi
+}
+
+switch_project() {
+  case ${PROJECT} in
+        FEMDK*)   echo FEMDK; link_local_cfg;;
+      Beafire*)   echo Beafire; link_local_cfg;;
+          IGA*)   echo IGA; link_local_cfg;;
+             *)   echo "Unknown project ${PROJECT}"; exit 22;;
+      esac
+}
+
+PROJECT=$1
+
+switch_project
